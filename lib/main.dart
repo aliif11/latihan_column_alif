2a3231c6dbed0c01412f47777728a3c3d092a40d
Alif M. Anwar Tambunan <88075284+aliif11@users.noreply.github.com>
2023-09-10 23:10:30 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -9,21 +9,21 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       // Application name
-      title: 'Flutter Hello World',
+      title: 'Latihan Column Alif',
       // Application theme data, you can set the colors for the application as
       // you want
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Latihan Column Alif'),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
   final String title;
-  const MyHomePage({super.key, required this.title});  
+  const MyHomePage({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
@@ -32,11 +32,115 @@ class MyHomePage extends StatelessWidget {
         // The title text which will be shown on the action bar
         title: Text(title),
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+      body: const Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.call,
+                size: 50,
+                color: Colors.blue,
+              ),
+              Text(
+                'Phone',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.near_me,
+                size: 50,
+                color: Colors.blue,
+              ),
+              Text(
+                'Route',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.share,
+                size: 50,
+                color: Colors.blue,
+              ),
+              Text(
+                'Share',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
 }
+
+// child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Icon(
+//               Icons.call,
+//               size: 50,
+//               color: Colors.blue,
+//             ),
+//             Text(
+//               'Call',
+//               style: TextStyle(fontSize: 20, color: Colors.brown),
+//             ),
+//           ],
+//         ),
+
+// body: const Center(
+//         child: Row(
+//           mainAxisAlignment: MainAxisAlignment.spaceAround,
+//           crossAxisAlignment: CrossAxisAlignment.center,
+//           children: [
+//             Icon(
+//               Icons.call,
+//               size: 50,
+//               color: Colors.blue,
+//             ),
+//             Icon(
+//               Icons.near_me,
+//               size: 50,
+//               color: Colors.blue,
+//             ),
+//             Icon(
+//               Icons.share,
+//               size: 50,
+//               color: Colors.blue,
+//             ),
+//           ],
+//         ),
+//       ),
+
+// child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             Icon(
+//               Icons.star,
+//               size: 48.0,
+//               color: Colors.yellow,
+//             ),
+//             SizedBox(height: 8.0), // Spasi antara ikon dan teks
+//             Text(
+//               'Teks di Bawah Ikon',
+//               style: TextStyle(fontSize: 18.0),
+//             ),
+//           ],
+//         ),
